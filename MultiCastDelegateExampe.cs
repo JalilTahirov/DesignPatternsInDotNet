690807e1c974597f1ef991d9678e1518469967f3
Jalil Tahrov <jalil.tahirov@outlook.com>
2018-11-26 00:45:51 +0400

    Create MultiCastDelegateExampe.cs

---
+++ MultiCastDelegateExampe.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ConsoleAppForDelegates
+{
+    public delegate void ProgressReporter(int percentComplete);
+    public class Util {
+        public static void HardWork(ProgressReporter p)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                p(i*10);                            //Invoke delegate
+                System.Threading.Thread.Sleep(100); //Simulate hard work
+            }
+        }
+
+        static void Main()
+        {
+            ProgressReporter p = WriteProgressToConsole;
+            p+= WriteProgressToFile;
+            Util.HardWork(p);
+
+        }
+
+        static void WriteProgressToConsole(int percentComplete)
+            =>Console.WriteLine(percentComplete);
+        
+        static void WriteProgressToFile (int percentComplete)
+            =>System.IO.File.WriteAllText("Progress.txt",percentComplete.ToString());
+
+    }
+
+
+}
